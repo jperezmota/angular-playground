942878b6340676fc7b2c51c18950928137fbc3fb
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-14 15:01:20 -0400

    Implemeting local reference example.

--- cmp-databinding-start/src/app/cockpit/cockpit.component.ts
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -11,25 +11,33 @@ export class CockpitComponent implements OnInit {
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   //
 
-  newServerName = '';
-  newServerContent = '';
+  /* Commented to use Local Reference instead. Discommented in case you will keep using ngModel on the view of this Component
+     and remember if you will do it to remover arguments and change implementation of onAddServer/onAddBlueprint
+  */
+  // newServerName = '';
+  // newServerContent = '';
+
+
+  // Assigning local reference to this property.
+  @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  onAddServer() {
+  //nameInput is the local reference which give your the whole html element.
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
-      serverName: this.newServerName,
-      serverContent: this.newServerContent
+      serverName: nameInput.value,
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
