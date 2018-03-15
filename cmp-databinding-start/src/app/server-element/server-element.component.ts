7eb9a2174906b2a84bc2254e51ca38f36303233b
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-14 22:24:03 -0400

    Implementing Component life cycle example.

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,18 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component,
+         OnInit,
+         Input,
+         ViewEncapsulation,
+         OnChanges,
+         SimpleChanges,
+         DoCheck,
+         AfterContentInit,
+         AfterContentChecked,
+         AfterViewInit,
+         AfterViewChecked,
+         OnDestroy,
+         ViewChild,
+         ElementRef,
+         ContentChild} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,13 +21,50 @@ import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
   // Changing View Encapsulation to let the Css being applied globally.
   encapsulation: ViewEncapsulation.Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, OnDestroy{
   // Using @Input to make the variable accessible for the outside world, so parent components can assign value to it.
   @Input('srvElement') element: {type: string, name:string, content: string};
+  @Input() name: string;
+  @ViewChild('header') header: ElementRef;
+  @ContentChild('contentParagraph') paragraph: ElementRef;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    console.log("constructor() called");
   }
 
+  ngOnChanges(changes: SimpleChanges){
+    console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log("ngOnInit() called");
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngDoCheck(){
+    console.log("ngDoCheck() called");
+  }
+
+  ngAfterContentInit(){
+    console.log("ngAfterContentInit() called");
+    console.log('Text content of paragraph: ' + this.paragraph.nativeElement.textContent);
+  }
+
+  ngAfterContentChecked(){
+    console.log("ngAfterContentChecked() called");
+  }
+
+  ngAfterViewInit(){
+    console.log("ngAfterViewInit() called");
+    console.log('Text content: ' + this.header.nativeElement.textContent);
+  }
+
+  ngAfterViewChecked(){
+    console.log("ngAfterViewChecked() called");
+  }
+
+  ngOnDestroy(){
+    console.log("ngOnDestroy() called");
+  }
 }
