a2089520f62d0bf90f696a8094b223518caa331d
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-15 15:54:24 -0400

    Implementing custom events and property bind to the custom directive better-highlight

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,13 +1,34 @@
-import { Directive, Renderer2, OnInit, ElementRef } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef, HostListener, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit{
+  @Input() defaultColor: string = 'transparent';
+  @Input() highLightColor: string = 'blue';
+
+  // Another way to set the style besides the renderer way implemented in the methods below. We can implement either.
+  // style.backgroundColor: Is the property of the html element to which we want to bind the backgroundColor property declared in this class.
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit(){
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.defaultColor;
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
+
+
+  // @HostListener: Decorated to listing events in the directive.
+  // mouseenter: Is the classic DOM Event.
+  @HostListener('mouseenter') mouseover(eventData: Event){
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    this.backgroundColor = this.highLightColor; // another way to do the same thing of the commented line above.
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    //this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = this.defaultColor; // another way to do the same thing of the commented line above.
+  }
+
 }
