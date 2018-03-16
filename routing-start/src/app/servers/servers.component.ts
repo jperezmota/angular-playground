b2399948979cdbec0c8681da7e2746f42c49965f
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-16 19:31:27 -0400

    Applying programatically navigation with relative path

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ServersService } from './servers.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-servers',
@@ -9,10 +10,18 @@ import { ServersService } from './servers.service';
 export class ServersComponent implements OnInit {
   private servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  /*
+    router: let you access the router so you can programatically navigate to differents urls.
+    route: inject the current activaly route
+  */
+  constructor(private serversService: ServersService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
+  }
+
+  onReload(){
+    this.router.navigate(['servers'], {relativeTo: this.route});
   }
 
 }
