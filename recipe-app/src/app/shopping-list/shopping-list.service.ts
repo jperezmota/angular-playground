d9d7e49a7498ab5f5592d4f0e9827ea79ee5f1ad
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-16 16:12:39 -0400

    Implementing communication to past recipe ingredients to shopping list

--- recipe-app/src/app/shopping-list/shopping-list.service.ts
+++ recipe-app/src/app/shopping-list/shopping-list.service.ts
@@ -17,4 +17,20 @@ export class ShoppingListService{
     this.ingredients.push(ingredient);
     this.ingredientsChanged.emit(this.ingredients.slice());
   }
+
+  addIngredients(ingredients: Ingredient[]){
+
+    /*
+    1st Aproach: Iterate and add them. But we can also follow the 2nd aproach below.
+    */
+    // for(let ingredient of ingredients){
+    //   this.addIngredient(ingredient);
+    // }
+
+    /*
+      2nd Aproach: Use Spread operator that is a feature of ES6. It converts an Array of elements to a List of elements.
+    */
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
