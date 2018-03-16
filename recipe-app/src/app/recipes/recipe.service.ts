5bc50e8bb4b61525e7450a880f376b1267e4571f
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-16 15:41:44 -0400

    Implementing Shopping List Service. Implemented Ingredients Items in the Recipe Detail

--- recipe-app/src/app/recipes/recipe.service.ts
+++ recipe-app/src/app/recipes/recipe.service.ts
@@ -1,18 +1,27 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService{
 
   recipeSelected = new EventEmitter<Recipe>();
 
   private recipes: Recipe []= [
-    new Recipe('A Test Recipe 1',
-               'This is simply a test 1',
-               'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    new Recipe('Tasty Schnitzel',
+               'A super Tasty Schnitzel just Awsome.',
+               'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+               [
+                 new Ingredient('Meat', 1),
+                 new Ingredient('French Fries', 20)
+               ]
             ),
-    new Recipe('A Test Recipe 2',
-               'This is simply a test 2',
-               'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+    new Recipe('Big Fat Burger',
+               'Your burger.',
+               'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+               [
+                 new Ingredient('Buns', 1),
+                 new Ingredient('Meat', 1)
+               ]
             )
   ];
 
