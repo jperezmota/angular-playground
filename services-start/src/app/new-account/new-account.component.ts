15339088c77c3590160e8c08955b72a78c18d220
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-15 22:11:04 -0400

    Implementing loggin service

--- services-start/src/app/new-account/new-account.component.ts
+++ services-start/src/app/new-account/new-account.component.ts
@@ -1,18 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LogginService } from 'app/login.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LogginService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  // The property type must be specified for the service.
+  constructor(private logginService: LogginService){}
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+
+    this.logginService.logStatusChange(accountStatus);
+
   }
 }
