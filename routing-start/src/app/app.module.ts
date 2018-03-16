494b1b6bac9627bef61215722be3aea9659bcf31
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-16 16:53:01 -0400

    Adding configuration in the app.module.ts to support routing

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -12,6 +12,17 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 
+import { Routes, RouterModule } from '@angular/router';
+
+/*
+  Constant for configure all the routes the app will be using.
+  The first with the empty string is for the case when the user do not put any resource. For example just localhost:4200
+*/
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'users', component: UsersComponent},
+  {path: 'servers', component: ServersComponent},
+];
 
 @NgModule({
   declarations: [
@@ -26,7 +37,8 @@ import { ServersService } from './servers/servers.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
