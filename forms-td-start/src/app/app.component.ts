683718ae4eeefb2f25ac1958395f1d35a6d0bf9c
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-21 12:32:09 -0400

    Submitting Form, accessing their value and presenting it.

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -13,6 +13,16 @@ export class AppComponent {
   answer = '';
   genders = ['male', 'female']
 
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+
+  submitted = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
     /* #1. setValue() Approach: Setting all the forms value input. The object properties must match with their input names and group
@@ -49,6 +59,14 @@ export class AppComponent {
 
   onSubmit(){
     console.log(this.signupForm);
+
+    this.submitted = true;
+    
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
   }
 
 }
