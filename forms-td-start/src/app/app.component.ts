ce886c513d2fb6aee982974815ac31a866b7bb26
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-21 12:17:44 -0400

    Implementing PatchValue() to change specific form element value

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -15,19 +15,30 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
-    /* Setting all the forms value input. The object properties must match with their input names and group names of the html.
-       Maybe this is not the best approach in this scenario because if the users already enter some values and we call this method
-       we will overide everything.
+    /* #1. setValue() Approach: Setting all the forms value input. The object properties must match with their input names and group
+       names of the html. Maybe this is not the best approach in this scenario because if the users already enter some values and
+       we call this method it will overide everything. Also it forces you to pass all the form data even if they are required or not.
     */
-    this.signupForm.setValue(
+    // this.signupForm.setValue(
+    //   {
+    //     userData: {
+    //       username: suggestedName,
+    //       email: ''
+    //     },
+    //     secret: 'pet',
+    //     questionAnswer: '',
+    //     gender: 'male'
+    //   }
+    // );
+
+    /*
+       #2. patchValue(). This approach let us overide specific form element, and not just the whole form values.
+    */
+    this.signupForm.form.patchValue(
       {
         userData: {
-          username: suggestedName,
-          email: ''
-        },
-        secret: 'pet',
-        questionAnswer: '',
-        gender: 'male'
+          username: suggestedName
+        }
       }
     );
   }
