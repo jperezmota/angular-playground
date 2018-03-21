cb040dde6bc728e57dc6c02c13e7ce62583980af
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-21 12:38:24 -0400

    Resetting form

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -61,12 +61,14 @@ export class AppComponent {
     console.log(this.signupForm);
 
     this.submitted = true;
-    
+
     this.user.username = this.signupForm.value.userData.username;
     this.user.email = this.signupForm.value.userData.email;
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 
 }
