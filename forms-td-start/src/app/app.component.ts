09a882de4cc085d59464efb384754aaef3de63e7
Jonathan Perez <jonathanperezmota@gmail.com>
2018-03-21 12:01:53 -0400

    Setting form values from the Component

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -15,6 +15,21 @@ export class AppComponent {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    /* Setting all the forms value input. The object properties must match with their input names and group names of the html.
+       Maybe this is not the best approach in this scenario because if the users already enter some values and we call this method
+       we will overide everything.
+    */
+    this.signupForm.setValue(
+      {
+        userData: {
+          username: suggestedName,
+          email: ''
+        },
+        secret: 'pet',
+        questionAnswer: '',
+        gender: 'male'
+      }
+    );
   }
 
   // onSubmit(form: NgForm){
