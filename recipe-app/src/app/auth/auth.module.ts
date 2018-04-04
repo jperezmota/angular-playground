43ad09bda993f8b80461edd4dd8be7f34bb9a1e6
Jonathan Perez <jonathanperezmota@gmail.com>
2018-04-04 00:09:27 -0400

    Restructuring service providers to auth.module

--- recipe-app/src/app/auth/auth.module.ts
+++ recipe-app/src/app/auth/auth.module.ts
@@ -3,6 +3,11 @@ import { SigninComponent } from "./signin/signin.component";
 import { SignupComponent } from "./signup/signup.component";
 import { FormsModule } from "@angular/forms";
 import { AuthRoutingModule } from "./auth-routing.module";
+import { ShoppingListService } from "../shopping-list/shopping-list.service";
+import { RecipeService } from "../recipes/recipe.service";
+import { DataStorageService } from "../shared/data-storage.service";
+import { AuthService } from "./auth.service";
+import { AuthGuard } from "./auth-guard.service";
 
 
 @NgModule({
@@ -13,7 +18,8 @@ import { AuthRoutingModule } from "./auth-routing.module";
   imports:[
     FormsModule,
     AuthRoutingModule
-  ]
+  ],
+  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
 })
 export class AuthModule{
 
